cf5a41fe23223ce04405aafb123c48ede422a7fc
Angel Ortmann-Li <ali02@stuy.edu>
2019-04-02 10:13:59 -0400

    end of class)

--- Radix.java
+++ Radix.java
@@ -4,11 +4,27 @@ public class radix{
     MyLinkedList<Integer> ans;
     MyLinkedList<Integer>[] buckets = new MyLinkedList<Integer>[10];
     int max = 0;
+    int dig = 0;
+    int[] hold = new int[data.length];
     for(int i : data){
       ans.add(i);
       if(i > max) max = i;
     }
-    
+    while(!(max < 10)){
+      max /= 10;
+      dig++;
+    }
+    for(int i = dig; i > 0; i--){
+      for(int x : data){
+        int a = digit(x,i);
+        buckets[a].add(x);
+      }
+      for(int i = 0; i < buckets.length; i++){
+        ans.extend(buckets[i]);
+      }
+      buckets.clear();
+    }
+
   }
 
   public int digit(int a, int b){
